84fcdc413ffde0f17b2fce96700824cf6390de70
Bianca Nenciu <nenciu.bianca@gmail.com>
2024-04-26 20:00:08 +0300

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1166,10 +1166,13 @@ Discourse::Application.routes.draw do
     get "/c", to: redirect(relative_url_root + "categories")
 
     resources :categories, only: %i[index create update destroy]
+    get "categories/:parent_category_id" => "categories#index",
+        :constraints => {
+          parent_category_id: /\d+/,
+        }
     post "categories/reorder" => "categories#reorder"
     get "categories/find" => "categories#find"
     get "categories/search" => "categories#search"
-    get "categories/:parent_category_id" => "categories#index"
 
     scope path: "category/:category_id" do
       post "/move" => "categories#move"
